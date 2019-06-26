2c8d8ccf57e34189e2c7f567f5a970223251abe8
Kabir Kwatra <kkwatra1@saddleback.edu>
2019-06-26 16:46:37 -0700

    added ImageScanner with implementaion, created Scanner Interface

--- src/main/java/hackweek/group/filterbot/TextScanner.kt
+++ src/main/java/hackweek/group/filterbot/TextScanner.kt
@@ -6,32 +6,36 @@ import net.dv8tion.jda.core.entities.Message
  * TextScanner is able to handle and scan messages
  * @param database database of filters
  */
-class TextScanner(private val database: Database) {
+class TextScanner(private val database: Database): Scanner<Message> {
 
     /**
      * Handles message by scanning message
      * and deleting if message if scan got a hit
-     * @param message Message to handle
+     * @param input Message to handle
      */
-    fun handle(message: Message) {
-        if (scan(message).first)
-            message.delete().queue()
+    override fun handle(input: Message) {
+        if (scan(input).first)
+            input.delete().queue()
     }
 
     /**
      *  Scans the message and
      * @return if message contains filter from database
-     * @param message Message to Scan
+     * @param input Message to Scan
      */
-    fun scan(message: Message): Pair<Boolean, String?> {
-        var text = message.contentStripped
+    override fun scan(input: Message): Pair<Boolean, FilterMatches?> {
+        val matches = FilterMatches()
+        var text = input.contentStripped
         if (!text.isNullOrEmpty()) {
             text = text.toLowerCase()
-            this.database.getFilters(message.guild.id).forEach {
-                if (text.contains(it.toLowerCase())) return Pair(true, it)
+            this.database.getFilters(input.guild.id).forEach {
+                if (text.contains(it.toLowerCase()))
+                    matches.add(
+                        Pair(it, 1.0)
+                    )
             }
         }
-        return Pair(false, null)
+        return Pair(matches.isNotEmpty(), if (matches.isNotEmpty()) matches else null)
 
     }
 }
