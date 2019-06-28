6fbd87274f6ee631a8a54dec8a4f1636df653b49
Kabir Kwatra <kabir@kwatra.me>
2019-06-28 01:53:34 -0700

    added newline

--- src/main/java/hackweek/group/filterbot/CommandManager.java
+++ src/main/java/hackweek/group/filterbot/CommandManager.java
@@ -51,6 +51,7 @@ public class CommandManager {
         if (messageWithoutPrefix.startsWith(Command.TEST.toString().toLowerCase())) return Command.TEST;
         if (messageWithoutPrefix.startsWith(Command.LIST.toString().toLowerCase())) return Command.LIST;
         if (messageWithoutPrefix.startsWith(Command.SETPREFIX.toString().toLowerCase())) return Command.SETPREFIX;
+        
         throw new IllegalStateException("Message: " + messageWithoutPrefix);
 
     }
